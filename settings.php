7cede3a94ff0a678832afe51f53491408bfaee3c
Emir <emiratesbp@gmail.com>
2015-04-07 14:44:55 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,143 @@
+<?php
+	$adresa[0]="11460 Kenyon Way Ste 108"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Rancho Cucamonga, CA 91701 ";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Rancho Cucamonga, CA";
+	$telephone[0]=9096520755;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="Plaza Rancho Penasquitos 9878 Carmel Mountain Rd. Ste C"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="San Diego, CA 92129";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="San Diego, CA";
+	$telephone[1]=8582407077;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="90 Mark West Springs Rd. Ste 100"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Santa Rosa, CA 95403";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Santa Rosa, CA";
+	$telephone[2]=7075263434;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="1708 Scenic Gulf Drive Miramar Beach"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Destin, FL 32550";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Destin, FL";
+	$telephone[3]=8502693997;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="327 Harbor Blvd"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Destin, FL 32541";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="Destin, Harbor Blvd.";
+	$telephone[4]=18884223277;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="1307 Third St. S"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="Naples, FL 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="Naples, FL";
+	$telephone[5]=2392130527;// Broj koji se poziva na dugme CALL US
+
+	$adresa[6]=" 1014 Underwood Avenue"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[6]="Pensacola, FL 32504";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[6]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[6]="Pensacola, FL";
+	$telephone[6]=8504780634;// Broj koji se poziva na dugme CALL US
+
+	$adresa[7]="17 Uptown Grayton Circle"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[7]="Santa Rosa Beach, FL 32459";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[7]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[7]="Santa Rosa Beach, FL";
+	$telephone[7]=18884223277;// Broj koji se poziva na dugme CALL US
+
+	$adresa[8]="3636 L. Honoapiilani Hwy "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[8]="Lahaina, HI 96761 ";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[8]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[8]="Lahaina, HI";
+	$telephone[8]=8007388223;// Broj koji se poziva na dugme CALL US
+
+	$adresa[9]="671 Front Street"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[9]="Lahaina, HI 96761";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[9]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[9]="Lahaina, HI, Front Street";
+	$telephone[9]=8086610942;// Broj koji se poziva na dugme CALL US
+
+	$adresa[10]="Iias Tsukuba 2F C50-1 Kenkyu-Gakuen Tsukuba-shi Lbaragi"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[10]="Iias Tsukuba, JP 305-0817";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[10]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[10]="Iias Tsukuba, JP";
+	$telephone[10]=18884223277;// Broj koji se poziva na dugme CALL US
+
+	$adresa[11]="Suruga-dai MK Build.1F 1-41-3 Kanda Jinbo-cho Chiyoda-ku, Tokyo "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[11]="Jinbo-cho, JP 101-0051";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[11]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[11]="Jinbo-cho, JP";
+	$telephone[11]=18884223277;// Broj koji se poziva na dugme CALL US
+
+	$adresa[12]="Kauhin Makuhari 1F 1-14 Hibiro Mihama-ku Chiba-shi Chiba"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[12]="Kaihin Makuhari, JP 261-0021";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[12]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[12]="Kaihin Makuhari, JP ";
+	$telephone[12]=18884223277;// Broj koji se poziva na dugme CALL US
+
+	$adresa[13]="Rusion Kannia 1F 1-3-11 Hagoromo-cho Naka-ku, Yokohama"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[13]="Kannai, JP 231-0047";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[13]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[13]="Kannai, JP";
+	$telephone[13]=18884223277;// Broj koji se poziva na dugme CALL US
+
+	$adresa[14]=" 135 South Atlantic St."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[14]="Dillon, MT 59725";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[14]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[14]="Dillon, MT";
+	$telephone[14]=4066836595;// Broj koji se poziva na dugme CALL US
+
+	$adresa[15]="965 F Highway 550 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[15]="Bernalillo, NM 87004";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[15]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[15]="Bernalillo, NM ";
+	$telephone[15]=5058671778;// Broj koji se poziva na dugme CALL US
+
+	$adresa[16]=""; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[16]="Las Vegas, NV";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[16]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[16]="Las Vegas, NV";
+	$telephone[16]=7022819632;// Broj koji se poziva na dugme CALL US
+
+	$adresa[17]="1571 Route 9 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[17]="Wappingers Falls, NY  12590";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[17]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[17]="Wappingers Falls, NY";
+	$telephone[17]=8456327060 ;// Broj koji se poziva na dugme CALL US
+
+	$adresa[18]="143 South Riverside Ave., Ste A"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[18]="Medford, OR 97501";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[18]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[18]="Medford, OR";
+	$telephone[18]=5412829660;// Broj koji se poziva na dugme CALL US
+
+	$adresa[19]="750 Crater Lake Avenue"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[19]="Medford, OR 97504";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[19]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[19]="Medford, OR, Crater Lake";
+	$telephone[19]=5417790974;// Broj koji se poziva na dugme CALL US
+
+	$adresa[20]="3530 S. State St."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[20]="Salt Lake City, UT 84115 ";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[20]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[20]="Salt Lake City, UT";
+	$telephone[20]=8012651182;// Broj koji se poziva na dugme CALL US
+
+	$adresa[21]="619 18th Street"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[21]="Virginia Beach, VA 23451 ";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[21]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[21]="Virginia Beach, VA";
+	$telephone[21]=8012651182;// Broj koji se poziva na dugme CALL US
+
+	$adresa[22]="Yacht Haven Grande 5330 Yacht Haven Grande Ste 103"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[22]="St. Thomas, VI 00802";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[22]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[22]="St. Thomas, VI";
+	$telephone[22]=3403442744;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=23;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slide"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
